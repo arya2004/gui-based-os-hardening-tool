29bbce98896712ba1313e2489986b59c386fbbb3
Soham Karandikar <sohamk10@gmail.com>
2023-09-13 20:53:04 +0530

    Added Conditional componnet

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,14 +1,27 @@
-import Button from "@renderer/components/Button"
+import { useState } from "react"
+import Conditional from "@components/Conditional"
 
-const executeScript = () => {
-  window.ipcRenderer.send('runScript')
-  console.log('Running Script')
-}
+// Some code for testing the Conditional component
 
 function App(): JSX.Element {
+  const [isScriptRunning, setIsScriptRunning] = useState(false)
+
+  const executeScript = () => {
+    window.ipcRenderer.send('runScript')
+    setIsScriptRunning(true)
+    setTimeout(() => {
+      setIsScriptRunning(false)
+    }, 3000)
+  }
+
   return (
+    
     <div className="container">
-      <Button onClick={() => console.log('hi mom')} text="hi bitch" />
+      <Conditional>
+        {/* If script is not running, render the button. Else, render the p tag */ }
+        <button slot="if" data-condition={!isScriptRunning} onClick={executeScript}>Run Script</button>
+        <p slot="else">Please wait, script is running...</p>
+      </Conditional>
     </div>
   )
 }
